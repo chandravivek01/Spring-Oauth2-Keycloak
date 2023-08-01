f9828c4e0c3b6d8e3c3914105627f728bbcb5257
Vivek Chandra Sardar <chandravivek52@gmail.com>
2023-08-01 21:39:25 +0530

    initial-commit

---
+++ src/test/java/com/vcs/authcode/SpringSecurityOauth2KeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcs.authcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2KeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
